d65bbb9a75b94ea2e06d081222f42894f0e3f36d
GCaringi <giannicaringi94@gmail.com>
2022-10-03 16:35:06 +0200

    Add view link on Header, create crontroller

---
+++ Views/Exercise/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace la_mia_pizzeria_static.Views.Exercise;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
